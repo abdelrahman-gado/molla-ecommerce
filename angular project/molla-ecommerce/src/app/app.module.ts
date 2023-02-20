ffe76c869ea94e094d5719d326742c98d49bf8db
Fayed <abdelrahman-fayed@hotmail.com>
2023-02-20 13:54:42 +0200

    frontEnd update

--- angular project/molla-ecommerce/src/app/app.module.ts
+++ angular project/molla-ecommerce/src/app/app.module.ts
@@ -3,10 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/01-header/header.component';
+import { FooterComponent } from './component/02-footer/footer.component';
+import { HomeComponent } from './component/03-home/home.component';
+import { ShopComponent } from './component/04-shop/shop.component';
+import { AboutComponent } from './component/05-about/about.component';
+import { FAQComponent } from './component/07-faq/faq.component';
+import { ContactComponent } from './component/06-contact/contact.component';
+import { CartComponent } from './component/08-cart/cart.component';
+import { WishlistComponent } from './component/09-wishlist/wishlist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    ShopComponent,
+    AboutComponent,
+    FAQComponent,
+    AboutComponent,
+    ContactComponent,
+    CartComponent,
+    WishlistComponent
   ],
   imports: [
     BrowserModule,
