f89350aa193971019a64a3b67493cd4d0e95d42c
Fayed <abdelrahman-fayed@hotmail.com>
2023-03-01 17:14:17 +0200

    Categories_home_update

--- angular project/molla-ecommerce/src/app/app.module.ts
+++ angular project/molla-ecommerce/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,6 +14,10 @@ import { ContactComponent } from './component/06-contact/contact.component';
 import { CartComponent } from './component/08-cart/cart.component';
 import { WishlistComponent } from './component/09-wishlist/wishlist.component';
 import { ProductDetailComponent } from './component/10-product-detail/product-detail.component';
+import { CategoriesComponent } from './component/11-categories/categories.component';
+import { ProductsComponent } from './component/12-products/products.component';
+import { CategoryComponent } from './component/11-categories/category/category.component';
+
 
 @NgModule({
   declarations: [
@@ -27,11 +32,15 @@ import { ProductDetailComponent } from './component/10-product-detail/product-de
     ContactComponent,
     CartComponent,
     WishlistComponent,
-    ProductDetailComponent
+    ProductDetailComponent,
+    CategoriesComponent,
+    ProductsComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular project/molla-ecommerce/src/app/component/01-header/header.component.ts
+++ angular project/molla-ecommerce/src/app/component/01-header/header.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component,OnInit } from '@angular/core';
+import { Category } from 'src/app/interface/category';
+import { CategoryService } from 'src/app/service/category/category.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+categories: Category[]=[]
+constructor(private categoryService:CategoryService){}
 
+ngOnInit(): void {
+    this.categoryService.getCategories().subscribe((data:any)=>{this.categories=data})
+}
 }
